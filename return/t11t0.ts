95b4397b1aa5fdbc3e582c6abc08d11f164d8ad0
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-03-21 18:12:06 +0800

    Add files via upload

---
+++ return/t11t0.ts
@@ -0,0 +1,12 @@
+let t11:any;
+let t0:any;
+void function t11t0(){
+    void t11[
+       `'w ^ Math.exp(NaN + t11) + x ^ Math.log(NaN - t0)',
+        'y ^ Math.exp(NaN - t11)' + 'z ^ Math.log(NaN + t0)'`
+    ]
+    void t0[
+       `'w ^ Math.exp(0 + t11)' + 'x ^ Math.log(0 - t0)',
+        'y ^ Math.exp(0 - t11)' + 'z ^ Math.log(0 + t0)'`
+    ]
+}
---
+++ return/time.ts
@@ -0,0 +1,24 @@
+import 't1t2.ts'
+import 't3t4.ts'
+import 't5t6.ts'
+import 't7t8.ts'
+import 't9t10.ts'
+import 't11t10.ts'
+ function t1t2(){
+     function t3t4(){
+         function t5t6(){
+             function t7t8(){
+                 function t9t10(){
+                     function t11t0(){
+                     return t1t2;
+                    }
+                  return t3t4;
+                }
+              return t5t6;
+            }
+            return t7t8;
+        }
+       return t9t10;
+    }
+    return t11t0;
+}

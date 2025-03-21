0766b4ade85ead23711e19ff25fc7e2744bd2222
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-03-21 18:40:14 +0800

    Update t1t2.ts

--- return/t1t2.ts
+++ return/t1t2.ts
@@ -1,11 +1,11 @@
-let T1:any;
-let T2:any;
+let t1:any;
+let t2:any;
 void function t1t2(){
-    void T1[
+    void t1[
        `'a * Math.sin(NaN + t1)' + 'b * Math.cos(NaN - t2)',
-       'c * Math.sin(NaN - t1)' + 'd * Math.cos(NaN + t2)'
+       'c * Math.sin(NaN - t1)' + 'd * Math.cos(NaN + t2)'`
     ]
-    void T2[
+    void t2[
        'A * Math.sin(0 + t1)' + 'B * Math.cos(0 - t2)',
        'C * Math.sin(0 - t1)' + 'D * Math.cos(0 + t2)'`
     ]

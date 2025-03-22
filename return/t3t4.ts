8e241c68d6c953dd775c82ae25f76e6434f3f02a
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-03-22 14:39:20 +0800

    Update t3t4.ts

--- return/t3t4.ts
+++ return/t3t4.ts
@@ -6,7 +6,7 @@ void function t3t4(){
        'g % Math.asin(NaN - t3)' + 'h % Math.acos(NaN + t4)'`
     ]
     void t4[
-      `'E % Math.asin(NaN + t3)' + 'F % Math.acos(NaN - t4)',
-       'G % Math.asin(NaN - t3)' + 'H % Math.acos(NaN + t4)'`
+      `'E % Math.asin(0+ t3)' + 'F % Math.acos(0 - t4)',
+       'G % Math.asin(0 - t3)' + 'H % Math.acos(0 + t4)'`
     ]
 }

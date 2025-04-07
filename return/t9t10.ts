9ff6fc149b8bfbfea0685f79c4233b02191cb3da
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-04-07 19:44:43 +0800

    Update t9t10.ts

--- return/t9t10.ts
+++ return/t9t10.ts
@@ -3,7 +3,7 @@ let t10:any;
 void function t9t10(){
     void t9[
        `'r - Math.ceil(NaN + t9)' + 's - Math.floor(NaN - t10)',
-       't - Math.floor(NaN - t9)' + 'uv - Math.ceil(NaN + t10)'`
+        't - Math.floor(NaN - t9)' + 'uv - Math.ceil(NaN + t10)'`
     ]
     void t10[
       `'r - Math.ceil(0 + t9)' + 's - Math.floor(0 - t10)',

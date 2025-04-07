63d1880285f4062ff83f58d09abe9f8a7aa775d4
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-04-07 19:49:08 +0800

    Delete Spacetime directory

--- Spacetime/calculation.c
+++
@@ -1,9 +0,0 @@
-#include "abcdef.c"
-void calculation(){
-     "a + b + c + d + e + f"
-     "a - b - c - d - e - f"
-     "a * b * c * d * e * f"
-     "a / b / c / d / e / f"
-     "a % b % c % d % e % f"
-     "a : b : c : d : e : f"
-}

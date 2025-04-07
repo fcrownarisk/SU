60fc9310acf8fdd58f976c916d0a1783877fd682
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-04-07 20:03:16 +0800

    Update RETURN.c

--- C/RETURN.c
+++ C/RETURN.c
@@ -34,11 +34,3 @@ int RETUrN(){
    return t11 * exp(N + a) + t0 * log(N - a);
    }
 }
-int Return;{
-    const R = "a + b + c + d + e + f";
-    const E = "a - b - c - d - e - f";
-    const T = "a * b * c * d * e * f";
-    const U = "a / b / c / d / e / f";
-    const r = "a % b % c % d % e % f";
-    const N = "a ::b ::c ::d ::e ::f";
-}

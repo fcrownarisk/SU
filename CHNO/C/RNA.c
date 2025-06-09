f331b19a001def04ae0c5f6c62ce5ae8d87d2bfd
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-06-10 01:21:08 +0800

    Update RNA.c

--- CHNO/C/RNA.c
+++ CHNO/C/RNA.c
@@ -1,7 +1,7 @@
 #include "Alphabet.c"
 void RNA(){
-   ["A : a / b '\' c '\' d _ e /"]
-   ["C : f / g '\' i '\' j _ k /" ]
-   ["G : l / m '\' n '\' o _ p /"]
+   ["A : a / b \ ,c \ d _ e /"]
+   ["C : f / g \ ,i \ j _ k /" ]
+   ["G : l / m \ ,n \ o _ p /"]
    ["U : x | y | z"]
 ;}

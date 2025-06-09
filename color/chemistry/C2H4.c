361f0f13bda5089560456d87a25b126b5c512818
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-06-10 01:30:38 +0800

    Update C2H4.c

--- color/chemistry/C2H4.c
+++ color/chemistry/C2H4.c
@@ -1,4 +1,12 @@
-void C2H4(float C, double H) {
-    double C2 = C*C;
-    double H2 = H*H + H*H;
+void C2H4(int C, int H) {
+    int C2 = C*C;
+    int H2 = H*H + H*H;
+}
+void C6H8(float C,float H) {
+    float C6 = C*C + C*C + C*C;
+    float H8 = H*H + H*H + H*H + H*H;
+}
+void C14H12(double C,double H) { 
+    double C14 = (C*C + C*C + C*C)*2 + C*C
+    double H12 = (H*H + H*H + H*H) * 2
 }

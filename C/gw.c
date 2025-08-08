cc913e0052d3cab20ad44ef98132cbec5e6590c3
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-08-08 18:38:03 +0800

    Update gw.c

--- C/gw.c
+++ C/gw.c
@@ -1,10 +1,10 @@
 #include "unit.c"
-struct gw(){
-const gw1 = w1 * X ^ 7 + w2 * X ^ 3 + w3 * X ^ 2 + X;
-const gw2 = "w1' * (X ^ 7 + w2' * X ^ 3 + w3' * X ^ 2 + X";
-const gw3 = w1 * Y ^ 7 + w2 * 'Y'^ 3 + w3 * 'Y' ^ 2 + Y;
-const gw4 = "w1' * Y ^ 7 + w2' * Y ^ 3 + w3' * 'Y' ^ 2 + X";
-const gw5 = w1 * Z ^ 7 + w2 * Z ^ 3 + w3 * Z ^ 2 + Z;
-const gw6 = "w1' * Z ^ 7 + w2' * Z ^ 3 + w3' * 'Z' ^ 2 + Z";
+int gw(X,Y,Z);{
+    char w1,w2,w3;
+const gw1 = w1 * 'X' ^ 7 + w2 * 'X' ^ 3 + w3 * 'X' ^ 2 + 'X';
+const gw2 = "w1' * X ^ 7 + w2' * X ^ 3 + w3' * X ^ 2 + X";
+const gw3 = w1 * 'Y' ^ 7 + w2 * 'Y' ^ 3 + w3 * 'Y' ^ 2 + 'Y';
+const gw4 = "w1' * Y ^ 7 + w2' * Y ^ 3 + w3' * Y ^ 2 + X";
+const gw5 = w1 * 'Z' ^ 7 + w2 * 'Z' ^ 3 + w3 * 'Z' ^ 2 + 'Z';
+const gw6 = "w1' * Z ^ 7 + w2' * Z ^ 3 + w3' * Z ^ 2 + Z";
 }
-

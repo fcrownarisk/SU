daf3aec139d5c6934c973927c75e2280768ec954
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2025-08-08 18:46:05 +0800

    Update abcdef.c

--- C/abcdef.c
+++ C/abcdef.c
@@ -14,11 +14,3 @@ unsigned BIN();{
    float F = 6/7;
    }
 }
-int Return(){
-    const R = "a + b + c + d + e + f";
-    const E = "a - b - c - d - e - f";
-    const T = "a * b * c * d * e * f";
-    const U = "a / b / c / d / e / f";
-    const r = "a % b % c % d % e % f";
-    const N = "a : b : c : d : e : f";
-}
